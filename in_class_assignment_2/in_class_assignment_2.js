3a0b637c5ac7dcbf049971e43ceebbee0fd5c001
Jenny Tran <work.trantran@gmail.com>
2025-02-11 16:16:24 -0500

    "Task 1 - Product Inventory Management"

---
+++ in_class_assignment_2/in_class_assignment_2.js
@@ -0,0 +1,23 @@
+// Task 1
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics" },
+    {name: "Phone", price: 800, category: "Electronics" },
+    {name: "Desk Chair", price: 150, category: "Furniture" },
+    {name: "Notebook", price: 1200, category: "Stationery" },
+    {name: "Headphones", price: 1200, category: "Electronics" },
+];
+
+function getProductByCategory(product, category) {
+    return product-array.filter(products => products.category);
+}
+
+console.log(getProductByCategory(products, "Electronics"));
+
+//Task 2
+
+function applyDiscount(products,discountRate) {
+    return products.map ()
+}
+
+console.log(sum3)
