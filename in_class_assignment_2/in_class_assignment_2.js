0f58d1455394392947edac0e14da17668912f87d
Jenny Tran <work.trantran@gmail.com>
2025-02-11 16:34:35 -0500

    "Task 3"

--- in_class_assignment_2/in_class_assignment_2.js
+++ in_class_assignment_2/in_class_assignment_2.js
@@ -23,3 +23,11 @@ function applyDiscount(products, discountRate) {
 }
 
 console.log(applyDiscount(products, 0.1));
+
+
+//Task 3
+let sales = [250, 400, 150, 900, 1200];
+function calculateTotalRevenue(sales) {
+    return sales.reduce ((total, sale)=>total+sale,0)
+}
+console.log('Total Rev. 2024: $${calculateTotalRevenue(sales)}')
